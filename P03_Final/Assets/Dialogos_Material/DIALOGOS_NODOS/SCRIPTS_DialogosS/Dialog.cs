0de1e417f4b2e6beb9c2c6251d2fc4d1b9445897
helenaLahora <127090172+helenaLahora@users.noreply.github.com>
2023-05-19 10:20:58 +0200

    REVERT

--- P03_Final/Assets/Dialogos_Material/DIALOGOS_NODOS/SCRIPTS_DialogosS/Dialog.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "newDialog", menuName = "DialogSystem/Dialog")]
-public class Dialog : ScriptableObject
-{
-    public string Name;
-    public DialogNode StartNode;
-}
-
